357f286b0ffc9e602338ef35c61a6fb26346032a
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-07-01 23:24:18 +0200

    Add prettier to eslint
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,23 @@
  */
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: [
-      "./tsconfig.json"
-    ]
-  },
-  plugins: [
-    "@typescript-eslint"
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
-  ]
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: [
+            "./tsconfig.json"
+        ]
+    },
+    plugins: [
+        "@typescript-eslint",
+        "prettier"
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ],
+    rules: {
+        'prettier/prettier': 'error'
+    }
 }
